7599d64be1baadd392fcacd5f2c4e15de4297036
stsullivan <stephen.sullivan@priceline.com>
2021-12-09 09:01:54 -0500

    1. Finished Day 4 refactor.

--- lib/day_four.ex
+++ lib/day_four.ex
@@ -8,20 +8,70 @@ defmodule DayFour do
     |> calc_winning_score
   end
 
+  def parse_input(file_name) do
+    Utilities.read_file_into_groups(file_name)
+    |> parse_string_groups
+  end
+
+  def parse_string_groups([numbers | cards]) do
+    draw = numbers |> String.split(",") |> Enum.map(&String.to_integer/1)
+
+    parsed_cards =
+      cards
+      |> Enum.map(&parse_card/1)
+      |> Enum.map(&setup_card/1)
+
+    [draw | parsed_cards]
+  end
+
+  def parse_card(card_string) do
+    rows = parse_rows(card_string)
+
+    rows ++ build_columns(rows)
+  end
+
+  defp parse_rows(card_string) do
+    card_string
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row -> String.split(row, " ", trim: true) end)
+    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+  end
+
+  defp build_columns(rows) do
+    rows
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def setup_card(card) do
+    card
+    |> Enum.map(fn series -> Enum.map(series, fn x -> {x, :o} end) end)
+  end
+
   def process_draws_to_last_card_win([draws | cards]) do
     draws
-    |> Enum.reduce_while(cards,
-        fn draw, cards ->
-          stamped_cards = Enum.map(cards, fn card ->stamp(card, draw) end)
-          case stamped_cards do
-            [last] -> if is_winner?(last) do
-                        {:halt, {draw, last}}
-                      else
-                        {:cont, [last]}
-                      end
-            ^stamped_cards -> {:cont, Enum.reject(stamped_cards, &is_winner?/1)}
-          end
-        end)
+    |> Enum.reduce_while(cards, &process_draw_for_loss/2)
+  end
+
+
+  def process_draw_for_loss(draw, cards) do
+    Enum.map(cards, fn card -> stamp(card, draw) end)
+    |> process_stamped_cards_for_loss(draw)
+
+  end
+
+  def process_stamped_cards_for_loss(stamped_cards, draw) do
+    case stamped_cards do
+      [last] -> process_last_card_for_loss(draw, last)
+      ^stamped_cards -> {:cont, Enum.reject(stamped_cards, &is_winner?/1)}
+    end
+  end
+
+  defp process_last_card_for_loss(draw, card) do
+    cond do
+      is_winner?(card) -> {:halt, {draw, card}}
+      true -> {:cont, [card]}
+    end
   end
 
   def play_for_win(file_name) do
@@ -63,41 +113,6 @@ defmodule DayFour do
     card_sum * draw
   end
 
-  def parse_string_groups([numbers | cards]) do
-    draw = numbers |> String.split(",") |> Enum.map(&String.to_integer/1)
-
-    parsed_cards =
-      cards
-      |> Enum.map(&parse_card/1)
-      |> Enum.map(&setup_card/1)
-
-    [draw | parsed_cards]
-  end
-
-  def parse_input(file_name) do
-    Utilities.read_file_into_groups(file_name)
-    |> parse_string_groups
-  end
-
-  def parse_card(card_string) do
-    rows =
-      card_string
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn row -> String.split(row, " ", trim: true) end)
-      |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
-
-    columns =
-      rows
-      |> Enum.zip()
-      |> Enum.map(&Tuple.to_list/1)
-
-    rows ++ columns
-  end
-
-  def setup_card(card) do
-    card
-    |> Enum.map(fn series -> Enum.map(series, fn x -> {x, :o} end) end)
-  end
 
   def stamp(card, draw) do
     card
