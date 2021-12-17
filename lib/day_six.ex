058cff7c467d5823e6f6d3ebc71d4b6597b68a41
stsullivan <stephen.sullivan@priceline.com>
2021-12-17 10:41:07 -0500

    1. Finished day six after refactor.

--- lib/day_six.ex
+++ lib/day_six.ex
@@ -1,12 +1,61 @@
 defmodule DaySix do
   @moduledoc false
 
-  def simulate_fish_population_from_file(file_name, days) do
 
+  def simulate_fish_population_from_file(file_name, days) do
+    buckets =
+      file_name
+      |> read_fish_from_file()
+      |> to_buckets
+
+      1..days
+      |> Enum.reduce(buckets, fn _, b -> decrement_all_buckets(b) end)
+      |> Enum.map(fn {_,count} -> count end)
+      |> Enum.sum
+  end
+
+  def decrement_all_buckets(buckets) do
+    buckets
+      |> Enum.map(&decrement/1)
+      |> List.flatten
+      |> Enum.group_by(fn {x,_} -> x end, fn {_,y} -> y end)
+      |> Map.to_list
+      |> Enum.map(fn {x,y} -> {x, Enum.sum(y)} end)
+      |> Enum.sort
+  end
+
+  def old_simulate_fish_population_from_file(file_name, days) do
+
+    fish = read_fish_from_file(file_name) |> Enum.chunk_every(1)
+
+    fish
+    |> Stream.map(fn f -> process_fish_for_days(f,days) end)
+    |> Enum.sum
+
+  end
+
+  def pass_one_day(buckets) do
+    buckets
+    |> Enum.map(&decrement/1)
+    |> List.flatten
+  end
+
+  def decrement({0,count}), do: [{6,count}, {8,count}]
+  def decrement({days,count}), do: {days-1,count}
+
+  def to_buckets(list) do
+    list
+    |> Enum.frequencies
+    |> Map.to_list
+    |> Enum.sort
+  end
+
+  defp process_fish_for_days(fish, days) do
     1..days
-    |> Enum.reduce(read_fish_from_file(file_name), fn _,fish_acc -> cycle(fish_acc, []) end)
+    |> Enum.reduce(fish, fn _,fish_acc -> cycle(fish_acc, []) end)
     |> Enum.count
   end
+
 
   defp read_fish_from_file(file_name) do
     file_name
--- test/day_six_test.exs
+++ test/day_six_test.exs
@@ -13,8 +13,55 @@ defmodule DaySixTest do
   [x] when not zero -> decrement
   [x] process test input for 18 days -> 26 fish
   [x] process test input for 80 days -> 5934 fish
-  [ ] process actual input for 80 days -> 5934 fishs
+  [x] process actual input for 80 days -> ? fish
+
+  [x] create buckets of fish by countdown key
+  [x] decrement buckets
+  [x] decrement zero day buckets
+  [x] decrement all buckets
+  [ ] iterate over buckets for n days
+  [ ] speed up algorithm
+
   """
+
+  test "process test input for 18 days -> 26 fish" do
+    assert "priv/day_six_test_input.txt" |> simulate_fish_population_from_file(18) == 26
+  end
+  test " process test input for 80 days -> 5934 fish" do
+    assert "priv/day_six_test_input.txt" |> simulate_fish_population_from_file(80) == 5934
+  end
+
+  test "decrement all buckets" do
+      assert decrement_all_buckets([{4,5},{0,2}]) == [{3,5}, {6,2}, {8,2}]
+      assert decrement_all_buckets([{3,5}, {6,2}, {8,2}])
+        == [{2,5}, {5,2}, {7,2}]
+      assert decrement_all_buckets([{7,5}, {5,2}, {0,3}])
+                                                 == [ {4,2}, {6,8}, {8,3}]
+  end
+
+  test "turned parse list into bucketss" do
+    assert to_buckets([3,4,3,1,2]) == [{1,1},{2,1},{3,2},{4,1}]
+  end
+
+  test "decrement buckets" do
+    assert decrement({1,1}) == {0,1}
+    assert decrement({0,3}) == [{6,3},{8,3}]
+  end
+
+
+
+
+  test "process buckets for one day" do
+    assert pass_one_day([{5,1},{0,2}]) == [{4,1},{6,2},{8,2}]
+  end
+
+
+  test "day six part two output" do
+    "priv/day_six_input.txt"
+    |> simulate_fish_population_from_file(256)
+    |> then(&"day six part 2 number of fish: #{&1}\n")
+    |> IO.puts
+  end
 
   test "day six part one output" do
     "priv/day_six_input.txt"
@@ -23,12 +70,8 @@ defmodule DaySixTest do
     |> IO.puts
   end
 
-  test " process test input for 80 days -> 5934 fish" do
-    assert "priv/day_six_test_input.txt" |> simulate_fish_population_from_file(80) == 5934
-  end
-
-  test "process test input for 18 days -> 26 fish" do
-    assert "priv/day_six_test_input.txt" |> simulate_fish_population_from_file(18) == 26
+  test " process test input for 256 days -> 26984457539 fish" do
+    assert "priv/day_six_test_input.txt" |> simulate_fish_population_from_file(256) == 26984457539
   end
 
 
