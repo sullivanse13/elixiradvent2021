da05d61cb84d2ca94dafd81e79d53b13326b4231
stsullivan <stephen.sullivan@priceline.com>
2021-12-20 09:06:20 -0500

    1. working day 8 part 2.

--- lib/day_eight.ex
+++ lib/day_eight.ex
@@ -2,6 +2,117 @@ defmodule DayEight do
   @moduledoc false
 
 
+  def decode(list, number) do
+    mapping = case number do
+      0 -> &find_zero/1
+      1 -> &find_one/1
+      4 -> &find_four/1
+      6 -> &find_six/1
+      7 -> &find_seven/1
+      8 -> &find_eight/1
+      9 -> &find_nine/1
+    end
+    mapping.(list)
+  end
+
+  def fetch(list, number) do
+    list
+    |> decode(number)
+    |> Map.fetch!(number)
+  end
+
+  def find_one(list), do: find_by_length(list, 2,1)
+  def find_four(list), do: find_by_length(list, 4,4)
+  def find_seven(list), do: find_by_length(list, 3,7)
+  def find_eight(list), do: find_by_length(list, 7,8)
+
+  def find_by_length(list, length, value) do
+    list
+    |> Enum.filter(fn s -> String.length(s) == length end)
+    |> hd
+    |> mapping(value)
+  end
+
+  def find_six(list) do
+    nine = fetch(list, 9)
+    zero = fetch(list, 0)
+    list
+    |> Enum.filter(fn s -> String.length(s) == 6 end)
+    |> Enum.reject(fn s -> s == nine end)
+    |> Enum.reject(fn s -> s == zero end)
+    |> hd
+    |> mapping(6)
+
+  end
+
+  def find_zero(list) do
+    nine = fetch(list, 9)
+    one = fetch(list, 1)
+
+    list
+    |> Enum.filter(fn s -> String.length(s) == 6 end)
+    |> Enum.reject(fn s -> s == nine end)
+    |> Enum.filter(fn s -> is_in?(s,one) end)
+    |> hd
+    |> mapping(0)
+
+  end
+
+  defp is_in?(container, containee) do
+    containee
+    |> String.codepoints
+    |> Enum.all?(fn c -> String.contains?(container, c) end)
+  end
+
+  def find_nine(list) do
+    four_code_points = fetch(list,4) |> String.codepoints
+    bottom = find_bottom(list) |> Map.fetch!(:b)
+    top = find_top(list) |> Map.fetch!(:t)
+
+    nine_characters = [top, bottom | four_code_points] |> Enum.join
+
+    list
+    |> Enum.filter(fn s -> contain_same_chars?(s,nine_characters) end)
+    |> hd
+    |> mapping(9)
+
+  end
+
+  def contain_same_chars?(a,b) do
+    Enum.sort(String.codepoints(a)) == Enum.sort(String.codepoints(b))
+  end
+
+  def find_top(list) do
+    one = Enum.filter(list, fn s -> String.length(s) == 2 end) |> hd
+    seven = Enum.filter(list, fn s -> String.length(s) == 3 end) |> hd
+
+    String.codepoints(seven) -- String.codepoints(one)
+    |> hd
+    |> mapping(:t)
+  end
+
+  def find_bottom(list) do
+
+    top = find_top(list) |> Map.fetch!(:t)
+
+    unknown_digits = list |> Enum.reject(fn s ->  Enum.member?([2,3,4],String.length(s)) end)
+    count = length(unknown_digits)
+
+    unknown_digits
+    |> Enum.map(&String.codepoints/1)
+    |> List.flatten
+    |> Enum.reject(fn s -> s == top end)
+    |> Enum.frequencies
+    |> Map.to_list
+    |> Enum.filter(fn {_k,v} -> v == count end)
+    |> hd
+    |> then(fn {bottom, _c} -> mapping(bottom,:b) end)
+
+  end
+
+  defp mapping(x, y), do: %{x => y, y => x}
+
+
   def count_easy_digits_from_file(file_name) do
     file_name
     |> Utilities.parse_file_lines_with(&parse_line/1)
--- test/day_eight_test.exs
+++ test/day_eight_test.exs
@@ -7,9 +7,73 @@ defmodule DayEightTest do
   TDD todo list
   [ ]
 
-  [x] parse line
-  [x] count strings of length 2,3,4,7 after pipe in line
+  [ ] parse line with code
+  [x] figure out top
+  [x] figure out bottom
+  [ ] figure out middle
+  [ ] figure out bottom left
+  [ ] figure out bottom right
+  [ ] figure out top left
+  [ ] figure out top right
+  [ ] figure out code
+  [x] find zero
+  [x] find one
+  [ ] find two
+  [ ] find three
+  [x] find four
+  [ ] find five
+  [ ] find six
+  [x] find seven
+  [x] find eight
+  [x] find nine
   """
+
+  @test_row ["acedgfb","cdfbe","gcdfa","fbcad","dab","cefabd","cdfgeb","eafb","cagedb","ab"]
+
+
+
+  test "find zero" do
+     @test_row |> find_zero() |> assert_code(0, "cagedb")
+  end
+
+  test "find six" do
+    @test_row |> find_six() |> assert_code(6, "cdfgeb")
+  end
+
+  test "find 9" do
+    @test_row |> find_nine() |> assert_code(9, "cefabd")
+  end
+
+  test "find easy numbers" do
+    @test_row |> find_one() |> assert_code(1, "ab")
+    @test_row |> find_four() |> assert_code(4, "eafb")
+    @test_row |> find_seven() |> assert_code(7, "dab")
+    @test_row |> find_eight() |> assert_code(8, "acedgfb")
+  end
+
+  def assert_code(input, number, string) do
+    input
+    |>  assert_map_equal(%{number=>string, string => number})
+  end
+
+#  test "figure out bottom left" do
+#    @test_row
+#    |> find_bottom_left()
+#    |> assert_map_equal(%{"g" => :bl, :bl => "g"})
+#
+#  end
+
+  test "figure out bottom" do
+    @test_row
+    |> find_bottom()
+    |> assert_map_equal(%{"c" => :b, :b => "c"})
+  end
+
+  test "figure out top" do
+    @test_row
+    |> find_top()
+    |> assert_map_equal(%{"d" => :t, :t => "d"})
+  end
 
   test "day eight part one" do
     "priv/day_eight_input.txt"
@@ -33,5 +97,10 @@ defmodule DayEightTest do
     assert count_easy_digit_size(["aa","fdb","fbgd","aaagcbe", "aaaaaa", "aaaaa"]) == 4
 
   end
+
+
+  # day
+  # [x] parse line
+  # [x] count strings of length 2,3,4,7 after pipe in line
 
 end
